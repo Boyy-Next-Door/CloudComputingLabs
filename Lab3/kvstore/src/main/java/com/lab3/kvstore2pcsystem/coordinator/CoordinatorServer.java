d726afdf8523da301006789d784704c3bece28f8
swg <suweigao1@gmail.com>
2020-05-26 08:52:36 +0800

    TODO:initialize

--- Lab3/kvstore/src/main/java/com/lab3/kvstore2pcsystem/coordinator/CoordinatorServer.java
+++ Lab3/kvstore/src/main/java/com/lab3/kvstore2pcsystem/coordinator/CoordinatorServer.java
@@ -1,10 +1,9 @@
 package com.lab3.kvstore2pcsystem.coordinator;
 
-import com.alibaba.fastjson.JSON;
 import com.lab3.kvstore2pcsystem.Participant;
 import com.lab3.kvstore2pcsystem.protocol.RespRequest;
 import com.lab3.kvstore2pcsystem.protocol.RespResponse;
-import com.lab3.kvstore2pcsystem.utils.HttpClientUtils;
+import com.lab3.kvstore2pcsystem.coordinator.RequestToPrepareRunner;
 import com.lab3.kvstore2pcsystem.utils.RespParseUtil;
 
 import java.io.*;
@@ -14,7 +13,6 @@ import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -108,11 +106,11 @@ class ExecuteThread implements Runnable {
                         break;
                     case GET:
                         //TODO 二阶段提交GET任务逻辑
-//                        doGET();
+                        doGET(respRequest);
                         break;
                     case DEL:
                         //TODO 二阶段提交DEL任务逻辑
-                        doDEL();
+                        doDEL(respRequest);
                         break;
                     default: //说明RESP报文请求类型不支持或错误 不做任何响应
                 }
@@ -127,8 +125,121 @@ class ExecuteThread implements Runnable {
 
     }
 
-    private void doDEL() {
+    private void doDEL(RespRequest respRequest) {
+//要向所有存活的参与者发送指令
+        ArrayList<Participant> participants = NodeManager.getAliveParticipantList();
+        //数据节点死光了  直接返回ERROR
+        if (participants.size() == 0) {
+            writeBackError();
+        }
+        //这里应该要开若干条线程  并行地通知所有参与者并等待其返回结果
+        ExecutorService executor = Executors.newCachedThreadPool();
+        ArrayList<FutureTask<RespResponse>> futures = new ArrayList<>();
+        for (Participant p : participants) {
+            FutureTask<RespResponse> futureTask = new FutureTask<RespResponse>(
+                    new RequestToPrepareRunner(p, respRequest, "DEL", ""));
+            futures.add(futureTask);
+            executor.submit(futureTask);
+        }
+        boolean abort = false;
+        try {
+            //检查所有参与者的返回情况
+            for (FutureTask<RespResponse> futureTask : futures) {
+                RespResponse respResponse = futureTask.get();
+                // 检查prepare-request的响应结果
+                if (respResponse == null || respResponse.getResponseType() != RespResponse.DEL_OK) {
+                    //SET的响应结果不是OK或者数据节点挂了 那么Abort这次任务
+                    abort = true;
+                    break;
+                }
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
 
+        //发送commit指令
+        if (abort) {
+            //SET指令回滚
+            futures = new ArrayList<>();
+            for (Participant p : participants) {
+                FutureTask<RespResponse> futureTask = new FutureTask<RespResponse>(
+                        new com.lab3.kvstore2pcsystem.coordinator.RequestToPrepareRunner(p, respRequest, "ROLLBACK", ""));
+                futures.add(futureTask);
+                executor.submit(futureTask);
+            }
+
+            boolean isDone = false;
+            RespResponse standardResponse = null; //从下面任取一个有效的response作为所有节点同一的返回内容
+            try {
+                //检查所有参与者的返回情况
+                for (FutureTask<RespResponse> futureTask : futures) {
+                    RespResponse respResponse = futureTask.get();
+                    // 检查commit的响应结果
+                    if (respResponse == null || respResponse.getResponseType() != RespResponse.DEL_ROLLBACK_DONE) {
+                        //数据节点挂了 或者 响应状态不是SET_ROLLBACK_DONE（这种情况理论上来说不可能出现）直接无视
+                        continue;
+                    } else {
+                        //响应结果为SET_ROLLBACK_DONE
+                        //只要有一个节点DONE了 说明最终结果是done 要做的只是等待所有响应结果都确定下来（要么done 要么挂掉）
+                        isDone = true;
+                        //取最后一个有效的respResponse
+                        standardResponse = respResponse;
+                    }
+                }
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+
+            if (isDone) {
+                //SET回滚指令提交成功 给客户端返回RESP响应SET失败的报文
+                writeBack(standardResponse);
+            } else {
+                //子节点全都挂掉了直接返回ERROR
+                writeBackError();
+            }
+
+        } else {
+            //SET指令提交
+            futures = new ArrayList<>();
+            for (Participant p : participants) {
+                FutureTask<RespResponse> futureTask = new FutureTask<RespResponse>(
+                        new RequestToPrepareRunner(p, respRequest, "COMMIT", ""));
+                futures.add(futureTask);
+                executor.submit(futureTask);
+            }
+
+            boolean isDone = false;
+            RespResponse standardResponse = null; //从下面任取一个有效的response作为所有节点同一的返回内容
+            try {
+                //检查所有参与者的返回情况
+                for (FutureTask<RespResponse> futureTask : futures) {
+                    RespResponse respResponse = futureTask.get();
+                    // 检查commit的响应结果
+                    if (respResponse == null || respResponse.getResponseType() != RespResponse.SET_COMMIT_DONE) {
+                        //数据节点挂了 或者 响应状态不是SET_COMMIT_DONE（这种情况理论上来说不可能出现）直接无视
+                        continue;
+                    } else {
+                        //响应结果为SET_COMMIT_DONE
+                        //只要有一个节点DONE了 说明最终结果是done 要做的只是等待所有响应结果都确定下来（要么done 要么挂掉）
+                        isDone = true;
+                        //取最后一个有效的respResponse
+                        standardResponse = respResponse;
+                    }
+                }
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+
+            if (isDone) {
+                //SET commit指令执行成功 给客户端返回RESP响应SET成功的报文
+                writeBack(standardResponse);
+            } else {
+                //子节点全都挂掉了直接返回ERROR
+                writeBackError();
+            }
+        }
+        //释放线程池资源
+        executor.shutdown();
     }
 
     private void doGET(RespRequest respRequest) {
@@ -346,4 +457,3 @@ class ExecuteThread implements Runnable {
     }
 
 }
-
